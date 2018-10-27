715008311af1374951891b85557adb9af8c33e7d
DarkEye <lesko.matej.pu@gmail.com>
2018-10-27 20:49:40 +0200

    first resolvers and binding

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,14 @@
-const mutations = {};
+const mutations = {
+  // return promise
+  async createItem(parent, args, ctx, info) {
+    // console.log(args);
+    // console.log(ctx);
+    // console.log(info);
+    // const item = await ctx.db.mutation.createItem({ data: { ...args } }, info);
+    // const item = await ctx.db.mutation.createItem({ data: { ...args } }, info);
+    const item = await ctx.db.mutation.createItem({ data: { ...args } });
+    return item;
+  }
+};
 
 module.exports = mutations;
