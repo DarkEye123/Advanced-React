abe0e2d8cd1cd846f5c4b1cfb5018b587b222eb2
DarkEye <lesko.matej.pu@gmail.com>
2018-11-04 23:20:46 +0100

    DeleteItem component with query FE

--- sick-fits/frontend/components/DeleteItem.js
+++ sick-fits/frontend/components/DeleteItem.js
@@ -1,7 +1,43 @@
 import React, { Component } from "react";
+import { Mutation } from "react-apollo";
+import gql from "graphql-tag";
+import { ALL_ITEMS_QUERY } from "./Items";
+
+const DELETE_ITEM_MUTATION = gql`
+  mutation DELETE_ITEM_MUTATION($id: ID!) {
+    deleteItem(id: $id) {
+      id
+    }
+  }
+`;
 
 export default class DeleteItem extends Component {
+  askDeletion(fn) {
+    if (confirm("Are you sure you want to delete this item?")) {
+      fn();
+    }
+  }
+  update(cache, payload) {
+    console.log(cache, payload);
+    const data = cache.readQuery({ query: ALL_ITEMS_QUERY });
+    data.items = data.items.filter(
+      ({ id }) => id !== payload.data.deleteItem.id
+    );
+    cache.writeQuery({ query: ALL_ITEMS_QUERY, data });
+  }
   render() {
-    return <button>{this.props.children}</button>;
+    return (
+      <Mutation
+        mutation={DELETE_ITEM_MUTATION}
+        variables={{ id: this.props.id }}
+        update={this.update}
+      >
+        {(deleteItem, { error }) => (
+          <button onClick={() => this.askDeletion(deleteItem)}>
+            {this.props.children}
+          </button>
+        )}
+      </Mutation>
+    );
   }
 }
