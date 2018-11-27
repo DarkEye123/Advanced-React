5fb0af98d445be0f3b733c59e420b639f5263a2b
DarkEye <lesko.matej.pu@gmail.com>
2018-11-28 00:46:55 +0100

    FE font change

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -45,12 +45,14 @@ const Inner = styled.div`
 
 injectGlobal`
 
-@font-face {
+/* @font-face {
   font-family: 'radnika_next';
   src: url('/static/radnikanext-medium-webfont.woff2') format('woff2');
   font-weight: normal;
   font-style: normal;
-}
+} */
+
+@import url('https://fonts.googleapis.com/css?family=Roboto');
 
 html {
   font-size: 10px;
@@ -66,13 +68,15 @@ body {
   margin: 0;
   font-size: 1.5rem;
   line-height: 2;
-  font-family: 'radnika_next';
+  /* font-family: 'radnika_next'; */
+  font-family: 'Roboto';
 }
 a {
   text-decoration: none;
   color: ${theme.black};
 }
- button {  font-family: 'radnika_next'; }
+ /* button {  font-family: 'radnika_next'; } */
+ button {  font-family: 'Roboto'; }
 `;
 
 export default class Page extends Component {
